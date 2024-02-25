d6359179677bd449197fa82c8f5b4b9f3d977020
Erik Liu <liu_jianying1985@hotmail.com>
2024-02-25 15:19:37 +0800

    city neighbours

--- chapter8-greedy/greedy.py
+++ chapter8-greedy/greedy.py
@@ -4,24 +4,28 @@
 # @Author   : juhao666
 # chapter 123 Greedy 贪婪算法
 
+# 用最少的电台尽可能覆盖所有州，剩下的电台就可以关闭了
 if __name__ == '__main__':
-    states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
+    # 这么多州
+    states_needed = {'mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'}
 
     # stations['k1'] = set(['id', 'av', 'ut'])
     # stations['k2'] = set(['wa', 'id', 'mt'])
     # stations['k3'] = set(['or', 'nv', 'ca'])
     # stations['k4'] = set(['nv', 'ut'])
     # stations['k5'] = set(['ca', 'az'])
-    stations = {'k1': set(['id', 'nv', 'ut']),
-                'k2': set(['wa', 'id', 'mt']),
-                'k3': set(['or', 'nv', 'ca']),
-                'k4': set(['nv', 'ut']),
-                'k5': set(['ca', 'az'])
+    # 每个电台k1,k2...都可以覆盖不同的州
+    stations = {'k1': {'id', 'nv', 'ut'},
+                'k2': {'wa', 'id', 'mt'},
+                'k3': {'or', 'nv', 'ca'},
+                'k4': {'nv', 'ut'},
+                'k5': {'ca', 'az'}
                 }
     final_stations = set()
+    # 州没处理完就一直跑
     while states_needed:
-        best_station = None # 用来存放最佳station
-        states_covered = [] # 用来保存覆盖最多的州列表
+        best_station = None  # 用来存放最佳station
+        states_covered = []  # 用来保存覆盖了最多的州列表
         # 遍历所欲stations 找到cover最多城市的那个station
         for station, states in stations.items():
             covered = states_needed & states
@@ -31,3 +35,5 @@ if __name__ == '__main__':
         states_needed -= states_covered
         final_stations.add(best_station)
     print(final_stations)
+
+    print(sorted(final_stations))
---
+++ chapter8-greedy/np_city_color.py
@@ -0,0 +1,69 @@
+# ！/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2024/2/25 13:46
+# @Author   : juhao666
+# 相邻城市颜色不同问题
+
+def find_max_neighbours(cb):
+    neighbour_count = 0
+    city_max_nb = None
+    for city, neighbours in cb.items():
+        if len(neighbours) > neighbour_count:
+            neighbour_count = len(neighbours)
+            city_max_nb = city
+    return city_max_nb
+
+
+if __name__ == '__main__':
+    cities = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    # city_neighbours['a'] = ['b', 'c', 'd']
+    # city_neighbours['b'] = ['a', 'c']
+    # city_neighbours['c'] = ['a', 'b', 'd', 'e']
+    # city_neighbours['d'] = ['a', 'c', 'e', 'g']
+    # city_neighbours['e'] = ['c', 'd', 'f']
+    # city_neighbours['f'] = ['e', 'g']
+    # city_neighbours['g'] = ['d', 'f']
+
+    city_neighbours = {'a': ['b', 'c', 'd'],
+                       'b': ['a', 'c'],
+                       'c': ['a', 'b', 'd', 'e'],
+                       'd': ['a', 'c', 'e', 'g'],
+                       'e': ['c', 'd', 'f'],
+                       'f': ['e', 'g'],
+                       'g': ['d', 'f']
+                       }
+    city_colors = {'a': None,
+                   'b': None,
+                   'c': None,
+                   'd': None,
+                   'e': None,
+                   'f': None,
+                   'g': None}
+
+    colors = {'red', 'yellow', 'white', 'green', 'pink', 'orange', 'brown'}
+    used_colors = set()
+    while city_neighbours:
+        # 找出邻居最多的那个city
+        city = find_max_neighbours(city_neighbours)
+        # 把这些邻居的Color放到一个set中
+        neighbours_colors = set([city_colors[key] for key in city_colors if key in city_neighbours[city]])
+        # 当前city的color不能是这些邻居的color
+        allowed_colors = colors - neighbours_colors
+        # 这块处理一个case:
+        # 如果某个颜色已经使用过，且当前邻居没有用，那就可以复用了
+        if len(used_colors - neighbours_colors) > 0:
+            # 拿到可以服用的color
+            allowed_colors = allowed_colors & used_colors
+        # 随便拿一个
+        # color = allowed_colors.pop()
+        # 每次都去第一个
+        color = sorted(allowed_colors)[0]
+        # 把这个color给当前city
+        city_colors[city] = color
+        # 新增一个使用的颜色到set
+        used_colors.add(color)
+        # 当前city已经有color了 可以从dict删除
+        del city_neighbours[city]
+    print(city_colors)
+    print(used_colors)
+    print(sorted(used_colors))
